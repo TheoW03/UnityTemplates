231a830e3929bfb853a27590d8b7a4d8bc108dbc
Theo <95878514+TheoW03@users.noreply.github.com>
2022-05-08 16:10:24 -0400

    Update SaveLoadClass.cs

--- SaveLoadClass.cs
+++ SaveLoadClass.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
+/**
+TODO: replace with stream reader
+*/
 public class SaveLoadClass<T>
 {
     private T Object;
@@ -15,12 +18,25 @@ public class SaveLoadClass<T>
     }
     public void setItem(T obj)
     {
-        if(!Directory.Exists(Application.persistentDataPath+"/mySaveData/")){
-            Directory.CreateDirectory("/mySaveData/");
+        // if (!Directory.Exists(Application.persistentDataPath + "/mySaveData/"))
+        // {
+        //     Directory.CreateDirectory("/mySaveData/");
+        // }
+        FileStream fcreate;
+        if (!File.Exists(Path.Combine(Application.persistentDataPath, this.fileName)))
+        {
+            fcreate = File.Open(Path.Combine(Application.persistentDataPath, this.fileName), FileMode.Create); // will create the file or overwrite it if it already exists
+            fcreate.Close();
         }
-        BinaryFormatter formatter = new BinaryFormatter();
-        string json = JsonUtility.ToJson(obj);
-        File.WriteAllText(Application.persistentDataPath +"/mySaveData/"+ this.fileName,json);
+
+        using (StreamWriter stream = new StreamWriter(Path.Combine(Application.persistentDataPath, this.fileName)))
+        {
+            string json = JsonUtility.ToJson(obj);
+            stream.Write(json);
+
+        }
+
+
     }
     /**
      (T),
@@ -29,17 +45,34 @@ public class SaveLoadClass<T>
     {
         try
         {
+            // if (!Directory.Exists(Application.persistentDataPath + "/mySaveData/"))
+            // {
+            //     Directory.CreateDirectory("/mySaveData/");
+            // }
+            using (StreamReader stream = new StreamReader(Path.Combine(Application.persistentDataPath, this.fileName)))
+            {
+                string json = stream.ReadToEnd();
+                Object = JsonUtility.FromJson<T>(json);
+                if (Object == null)
+                {
 
-            string json = File.ReadAllText(Application.persistentDataPath +"/mySaveData/"+ this.fileName);
-            Object = JsonUtility.FromJson<T>(json);
+                    return default(T);
+                }
+                return Object;
+            }
+
+
         }
         catch (FileNotFoundException e)
         {
             Debug.Log("error getting data");
+            return default(T);
         }
-
-
-        return Object;
+        catch (IOException e)
+        {
+            Debug.Log("file maybe corrupted");
+            return default(T);
+        }
     }
 
 }
