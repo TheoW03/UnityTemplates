adfa37f56e44c4dcf160b81c87158dbdd179c51e
Theo <95878514+TheoW03@users.noreply.github.com>
2022-07-31 15:17:05 -0400

    Create shaderStuff.cpp

---
+++ shaderStuff.cpp
@@ -0,0 +1,66 @@
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+using namespace std;
+#define print(C) cout<<C<<endl
+using std::filesystem::current_path;
+static int compileShader(unsigned int type, const string& source) {
+    unsigned int id =  glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id,1,&src,nullptr);
+    glCompileShader(id);
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result); 
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length,&length, message);
+        print(message);
+    }
+    return id;
+
+}
+static unsigned int CreateShader(const string& vertexSahder, const string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexSahder);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    glAttachShader(program, fs);
+    glAttachShader(program, vs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    glDeleteShader(fs);
+    glDeleteShader(vs);
+   
+    return program;
+
+}
+string getShaderString(string path) {
+    ifstream inFile;
+    string tp;
+    inFile.open(path);
+    if (!inFile)
+    {
+      
+        return "error";
+    }
+    string fContent = "";
+    if (inFile.is_open())
+    { // checking whether the file is open
+        string t = string((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
+        inFile.close();
+        return t;
+       
+    }
+    return "error";
+   
+
+
+}
